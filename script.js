2b849bff7cca077c2888d3515c4929dc8b11f8ca
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-09-14 19:19:24 +0530

    'first'

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 'use strict';
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+import { getDatabase, ref, get, set, child, onValue, off }
+    from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js';
+
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
@@ -6,10 +11,6 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
@@ -17,84 +18,209 @@ let currentScore = 0;
 let activePlayer = 0;
 let scores = [0, 0];
 let playing = true;
+let lobby = 0;
+var mpBtnDisabled = false;
+var selfPlayer = 0;
 
-// Switching players function
+class MP {
+    static initialized = false;
+    static connected = false;
+    static db = null;
+    static firstRun = true;
+    static waiting = true;
+
+    static init() {
+        if (this.initialized) return;
+
+        initializeApp({
+            apiKey: "AIzaSyDJRYKceU0s4JGKGh9so2APSFlHUWWuTn0",
+            authDomain: "roll-a-dice-247.firebaseapp.com",
+            databaseURL: "https://roll-a-dice-247-default-rtdb.asia-southeast1.firebasedatabase.app",
+            projectId: "roll-a-dice-247",
+            storageBucket: "roll-a-dice-247.appspot.com",
+            messagingSenderId: "553490756756",
+            appId: "1:553490756756:web:23cfa21c8b518528dcf081"
+        });
+        MP.db = getDatabase();
+        MP.initialized = true;
+    }
+
+    static connect() {
+        if (mpBtnDisabled)
+            return;
+
+        newGame();
+        playing = false;
+        mpBtnDisabled = true;
+        document.querySelector(".btn--mp").innerHTML = "⌛ Connecting...";
+        
+        if (MP.firstRun) {
+            MP.init();
+            MP.firstRun = false;
+        }
+
+        // find a lobby
+        get(child(ref(MP.db), 'mm')).then(snapshot => {
+            var mminfo = { next: 1, waiting: false };
+            if (!snapshot.exists())
+                set(ref(MP.db, 'mm'), mminfo);
+            else
+                mminfo = snapshot.val();
+        
+            lobby = mminfo.next;
+        
+            var mminfoNew = mminfo;
+            if (mminfo.waiting) {
+                mminfoNew.next++;
+                selfPlayer = 1;
+                MP.waiting = false;
+            } else {
+                selfPlayer = 0;
+                MP.waiting = true;
+            }
+            mminfoNew.waiting = !mminfo.waiting;
+            set(ref(MP.db, 'mm'), mminfoNew);
+            MP.connected = true;
+
+            // join (set up) that lobby
+            set(ref(MP.db, `${lobby}/data/j${selfPlayer}`), true);
+            document.getElementById(`name--${selfPlayer}`).innerHTML = "YOU"
+            onValue(ref(MP.db, `${lobby}/moves/p${1-selfPlayer}`), snapshot => {
+                if (!snapshot.exists()) return;
+
+                if (snapshot.val() === 'hold')
+                    hold();
+                else if (snapshot.val().startsWith('roll'))
+                    roll(parseInt(snapshot.val().substring(5)));
+
+            });
+
+            if (selfPlayer === 0) {
+                document.querySelector(".btn--mp").innerHTML = "⌛ Waiting...";
+                onValue(ref(MP.db, `${lobby}/data`), snapshot => {
+                    if (!snapshot.exists()) return;
+                    if (MP.waiting) {
+                        if (snapshot.val()[`j${1-selfPlayer}`]) {
+                            MP.waiting = false;
+                            document.querySelector(".btn--mp").innerHTML = "🎲 Joined";
+                            playing = true;
+                            off(ref(MP.db, `${lobby}/data`));
+                        } else return;
+                    }
+                })
+            } else {
+                document.querySelector(".btn--mp").innerHTML = "🎲 Joined";
+                playing = true;
+            }
+        })
+    }
+
+    static sendMoveHold() {
+        set(ref(MP.db, `${lobby}/moves/p${selfPlayer}`), 'hold');
+    }
+
+    static sendMoveRoll(num) {
+        set(ref(MP.db, `${lobby}/moves/p${selfPlayer}`), `roll ${num}`);
+    }
+
+    static disconnect() {
+        if (!MP.connected) return;
+
+        document.querySelector(".btn--mp").innerHTML = "⌛ Disconnecting...";
+        off(ref(MP.db, `${lobby}/moves/p${1-selfPlayer}`));
+        ref(MP.db, `${lobby}`).delete();
+        document.querySelector(".btn--mp").innerHTML = "🎲 Multiplayer";
+        MP.connected = false;
+    }
+};
+
 function switchPlayer() {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  currentScore = 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = 1 - activePlayer;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
 }
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+function roll(preset = 0) {
+    if (playing) {
+        // Generating random dice roll
+        const dice = (preset == 0) ? (Math.trunc(Math.random() * 6) + 1) : preset;
 
-// Rolling dice functionality
-btnRoll.addEventListener('click', function () {
-  if (playing) {
-    // Generating random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
+        // Display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
 
-    // Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+        // Check fir rolled 1
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore = currentScore + dice;
+            document.getElementById(`current--${activePlayer}`).textContent =
+                currentScore;
+        } else {
+            // Switch player
+            switchPlayer();
+        }
 
-    // Check fir rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore = currentScore + dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
-    } else {
-      // Switch player
-      switchPlayer();
+        return dice;
     }
-  }
-});
+}
 
-// Hold score functionality
-btnHold.addEventListener('click', function () {
-  if (playing) {
-    // Adding current score to active player's score
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+function hold() {
+    if (playing) {
+        // Adding current score to active player's score
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent =
+            scores[activePlayer];
 
-    // Checking score>=100 and finish game
-    if (scores[activePlayer] >= 100) {
-      playing = false;
-      diceEl.classList.add('hidden');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
-    } else {
-      //Switch player
-      switchPlayer();
+        // Checking score>=100 and finish game
+        if (scores[activePlayer] >= 100) {
+            playing = false;
+            diceEl.classList.add('hidden');
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.add('player--winner');
+            document
+                .querySelector(`.player--${activePlayer}`)
+                .classList.remove('player--active');
+        } else {
+            switchPlayer();
+        }
     }
-  }
+}
+
+function newGame() {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    currentScore = 0;
+    document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--winner');
+    activePlayer = 0;
+    scores = [0, 0];
+    MP.disconnect();
+    playing = true;
+    mpBtnDisabled = false;
+    diceEl.classList.add('hidden');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+document.getElementById('btnNew').addEventListener('click', newGame);
+document.getElementById('btnMp').addEventListener('click', MP.connect);
+document.getElementById('btnRoll').addEventListener('click', () => {
+    if (activePlayer === selfPlayer)
+        MP.sendMoveRoll(roll())
+});
+document.getElementById('btnHold').addEventListener('click', () => {
+    if (activePlayer === selfPlayer) {
+        hold();
+        MP.sendMoveHold();
+    }
 });
 
-// New game functionality
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  currentScore = 0;
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-  activePlayer = 0;
-  scores = [0, 0];
-  playing = true;
-  diceEl.classList.add('hidden');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
-});
+window.addEventListener('unload', MP.disconnect());
+
+newGame();
